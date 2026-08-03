23c126781c3c4f8e093e43404cf785c1491dc6fa
Parakh24 <parakhvirnawe2404@gmail.com>
2026-08-03 12:03:05 +0530

    this file uses database logic to fetch and push the data to the database

--- app/feature/image_generation/repositories/generation_jobs.py
+++ app/feature/image_generation/repositories/generation_jobs.py
@@ -41,4 +41,64 @@ class GenerationJobRepository:
         return job 
 
 
-      
+
+      def get_job_by_id(self, job_id: str) -> Optional[GenerationJob]:
+        """
+         This function fetches one of the job ids to check whether the tables
+         exist on the database server or not. If not, then service decides whether 
+         to send an error or to retry it.
+        """
+
+        return self.db.query(GenerationJob).filter(GenerationJob.id == job_id).first() 
+
+
+
+      def update_job_status(
+            self, job_id: str, status: GenerationStatus
+      ) -> Optional[GenerationJob]: 
+         """
+         Generic status change -> like "PENDING" -> "PROCESSING" only when worker picks up the job 
+         """
+         job = self.get_job_bby_id(job_id) 
+
+         if job is None: 
+            return None 
+
+         job.status = status 
+         self.db.commit() 
+         self.db.refresh(job) 
+         return job 
+
+
+
+      def mark_job_completed(self, job_id: str) -> Optional[GenerationJob]: 
+         """
+         It sets the job as completed. Call this only when the image asset row is created. 
+         """
+         job = self.get_job_by_id(job_id) 
+         if job is None: 
+            return None 
+
+         job.status = GenerationStatus.COMPLETED 
+         job.completed_at = datetime.now(timezone.utc) 
+         self.db.commit() 
+         self.db.refresh(job) 
+         return job 
+
+
+
+      def mark_job_failed(self, job_id: str, error_message: str) -> Optional[GenerationJob]:
+         """
+         It sets the job as FAILED and records the reason behind this
+         """
+         job = self.get_job_by_id(job_id) 
+
+         if job is None: 
+            return None 
+
+         job.status = GenerationStatus.FAILED 
+         job.error_message = error_message
+         job.completed_at = datetime.now(timezone.utc) 
+         self.db.commit() 
+         self.db.refresh(job) 
+         return job 
