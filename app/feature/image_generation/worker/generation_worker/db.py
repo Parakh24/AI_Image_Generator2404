343e4b0b396ebf5c34b1a15a0473d72288fe1943
Parakh24 <parakhvirnawe2404@gmail.com>
2026-08-03 13:23:43 +0530

    db file restarts the session everytime the worker fetches a new job id since worker works process runs on a separate thread

---
+++ app/feature/image_generation/worker/generation_worker/db.py
@@ -0,0 +1,12 @@
+# worker/generation_worker/db.py 
+
+from app.database import SessionLocal 
+
+def get_worker_session(): 
+    """
+    Returns a new SQLAlchemy session for the worker process. 
+    This function creates a new session because the worker process runs in 
+    a separate thread from the main FASTAPI app, and we want to avoid sharing sessions 
+    across threads. """
+
+    return SessionLocal() 
