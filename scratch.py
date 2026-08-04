89723cf4255c50cf823d0d06906e5aeedeb03cbe
Parakh24 <parakhvirnawe2404@gmail.com>
2026-08-04 12:41:07 +0530

    this file is used for testing purposes

---
+++ scratch.py
@@ -0,0 +1,14 @@
+from app.feature.image_generation.services.prompt_service import prompt_service, PromptRequest, PromptPreset, BusinessProfile 
+
+request = PromptRequest(
+    user_prompt = "Create a social media advertisement for a new product launch,",
+    business_profile = BusinessProfile(
+        brand_name="ABC",
+        primary_color="blue",
+        audience_demographics="students",
+    ),
+    preset = PromptPreset.SOCIAL_MEDIA_AD,
+)
+
+final_prompt = prompt_service.compile(request) 
+print(final_prompt) 
