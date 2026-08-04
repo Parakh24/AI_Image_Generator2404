1fda40ed368aa0228f3690ed212ee90aa4f244eb
Parakh24 <parakhvirnawe2404@gmail.com>
2026-08-04 11:44:41 +0530

    worker and routes use this file to access the prompt

---
+++ app/feature/image_generation/services/prompt_service/service.py
@@ -0,0 +1,19 @@
+from app.feature.image_generation.services.prompt_service.builder import build_final_prompt
+from app.feature.image_generation.services.prompt_service.schemas import PromptRequest 
+
+"""
+This file is majorly used by the api/routes service or worker service, they do not need to enter into 
+the details of how the prompt is built, they just need to call this service and get the final prompt.
+"""
+
+
+class PromptService: 
+    def compile(self, request: PromptRequest) -> str: 
+        return build_final_prompt(
+                user_prompt=request.user_prompt,
+                business_profile=request.business_profile,
+                preset= request.preset,
+        )
+
+
+prompt_service = PromptService() 
